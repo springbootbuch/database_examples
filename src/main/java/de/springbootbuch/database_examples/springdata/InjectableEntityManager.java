20c4f131e02529b49d6416a2ecc2940ced69f266
Michael J. Simons <michael@simons.ac>
2017-04-10 22:40:52 +0200

    Fix broken test

--- src/main/java/de/springbootbuch/database_examples/springdata/InjectableEntityManager.java
+++ src/main/java/de/springbootbuch/database_examples/springdata/InjectableEntityManager.java
@@ -2,6 +2,7 @@ package de.springbootbuch.database_examples.springdata;
 
 import java.util.List;
 import javax.persistence.EntityManager;
+import org.springframework.stereotype.Repository;
 
 /**
  * Part of springbootbuch.de.
@@ -11,6 +12,7 @@ import javax.persistence.EntityManager;
  * @author Michael J. Simons
  * @author @rotnroll666
  */
+@Repository
 public class InjectableEntityManager {
 	
 	private final EntityManager entityManager;
