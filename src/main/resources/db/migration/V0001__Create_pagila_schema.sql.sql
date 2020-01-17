17b0fe9dcb5aa26917e712a527fb2f3d05c1e9f7
schokominzpfefferlade <60002031+schokominzpfefferlade@users.noreply.github.com>
2020-01-17 18:07:15 +0100

    Update V0001__Create_pagila_schema.sql.sql (#2)
    fix for #1, Docker Postgres 9.4 failure

--- src/main/resources/db/migration/V0001__Create_pagila_schema.sql.sql
+++ src/main/resources/db/migration/V0001__Create_pagila_schema.sql.sql
@@ -8,6 +8,8 @@ SET check_function_bodies = false;
 SET client_min_messages = warning;
 SET escape_string_warning = off;
 
+CREATE ROLE postgres;
+
 --
 -- Name: SCHEMA public; Type: COMMENT; Schema: -; Owner: postgres
 --
